598809edb1f9a7ab012896b0233d77e0048421bf
Vítor Alves <alves.vitor@bcg.com>
2023-03-28 18:50:40 -0300

    routing views home view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AnalysisView from './modules/analysis/View';
+import CalendarView from './modules/calendar/View';
 import AppBar from './modules/core/components/app-bar/AppBar';
+import HomeView from './modules/home/View';
 
 const theme = createTheme({
   palette: {
@@ -15,6 +18,11 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <AppBar></AppBar>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/calendar" element={<CalendarView />} />
+          <Route path="/analysis" element={<AnalysisView />} />
+        </Routes>
       </ThemeProvider>
     </div>
   );
---
+++ src/modules/home/View.tsx
@@ -0,0 +1,82 @@
+import { CardActionArea } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { ReactComponent as PaperIcon } from './../../paper.svg';
+import { ReactComponent as SpeedometerIcon } from './../../speedometer.svg';
+
+export function ActionAreaCard({
+  margin,
+  title,
+  description,
+  icon,
+}: {
+  margin?: string;
+  title: string;
+  description: string;
+  icon: any;
+}) {
+  return (
+    <Card sx={{ maxWidth: 345, margin }}>
+      <CardActionArea sx={{ padding: 4 }}>
+        {icon()}
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            fontWeight="500"
+          >
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default function View() {
+  const cards = [
+    {
+      title: 'Calendar optimization',
+      description:
+        'Create a new calendar from the future or update / re-optimize your current plan',
+      icon: () => <PaperIcon></PaperIcon>,
+    },
+    {
+      title: 'Post-event analysis',
+      description:
+        'Review the performance of past promotions based on uplifts and financial KPIs',
+      icon: () => <SpeedometerIcon></SpeedometerIcon>,
+    },
+  ];
+
+  const renderCards = () =>
+    cards.map((card, key) => {
+      return (
+        <ActionAreaCard
+          key={key}
+          margin="5px 20px"
+          description={card.description}
+          title={card.title}
+          icon={card.icon}
+        ></ActionAreaCard>
+      );
+    });
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        direction: 'ltr',
+        alignItems: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      {renderCards()}
+    </div>
+  );
+}
